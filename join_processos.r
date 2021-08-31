4be1e32244ae52b5a23807ea3a288c914cc6698a
Renato Chavez <67420733+Renatochaz@users.noreply.github.com>
2021-08-31 16:42:54 -0300

    Update join_processos.r

--- join_processos.r
+++ join_processos.r
@@ -1,15 +1,14 @@
 ## joing web scrapping with operacoes-ajustado
 library(dplyr)
-bndes<-read.csv("/Users/rondon/Downloads/operacoes-ajustado.csv",sep = ",",encoding = "UTF-8" )
+bndes<-read.csv("bases-ajustadas/operacoes-ajustado.csv",sep = ",",encoding = "UTF-8" )
 cnpjs<-unique(bndes$cnpj)
 cnpjs<-gsub('[-]','',cnpjs)
 cnpjs<-gsub('[./]','',cnpjs)
 
-biz<-read.csv("/Users/rondon/Downloads/empresas_processos.xlsx - empresas_processos.csv",header = FALSE)
+biz<-read.csv("bases-auxiliares/empresas.csv",header = FALSE)
 names(biz)<-c("cliente","processos")
-write.csv(biz,"/Users/rondon/Downloads/enterprises.csv",row.names=FALSE)
 
 biz$processos<-gsub('processo.*','',biz$processos)
 biz$processos<-gsub('[.]','',biz$processos)
 bndesf<-left_join(bndes,biz,by="cliente")
-write.csv(bndesf,"/Users/rondon/Downloads/bndes_processos2.csv",row.names=FALSE)
+write.csv(bndesf,"/bases-ajustadas/bndes_processos2.csv",row.names=FALSE)
